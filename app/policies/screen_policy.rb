17adbb2a6985ccc9add885c09f62b54d234e1c9b
Brian Michalski <bmichalski@gmail.com>
2025-11-15 12:38:14 -0500

    Refactor screen policy methods for clarity.

--- app/policies/screen_policy.rb
+++ app/policies/screen_policy.rb
@@ -21,58 +21,74 @@ class ScreenPolicy < ApplicationPolicy
   end
 
   def new?
-    super || begin
-      # Screens can be created by any admin of any group.
-      return false unless user
-      user.admin_groups.any?
-    end
+    super || can_create_new_screen?
   end
 
   def create?
-    super || begin
-      # Screens can be created by any admin of the associated group.
-      return false unless user
-      record.group.admin?(user)
-    end
+    super || can_create_screen?
   end
 
   def edit?
-    super || begin
-      # Screens can be edited by any member of the associated group.
-      return false unless user
-      record.group.member?(user)
-    end
+    super || can_edit_screen?
   end
 
   def update?
-    super || begin
-      return false unless edit?
-
-      # If group_id is being changed, ensure user is admin of both
-      # the current group and the new group.
-      if record.group_id_changed?
-        # Check permissions on the old group, if it existed.
-        if record.group_id_was.present?
-          old_group = Group.find_by(id: record.group_id_was)
-          return false unless old_group&.admin?(user)
-        end
-
-        # Check permissions on the new group.
-        # The `record` is already associated with the new group object.
-        return false unless record.group&.admin?(user)
-      end
-
-      true
-    end
+    super || can_update_screen?
   end
 
   def destroy?
-    super || begin
-      # Screens can be deleted by any admin of the associated group.
-      return false unless user
-      record.group.admin?(user)
-    end
+    super || can_destroy_screen?
   end
+
+  private
+
+  # Screens can be created by any admin of any group.
+  def can_create_new_screen?
+    return false unless user
+    user.admin_groups.any?
+  end
+
+  # Screens can be created by any admin of the associated group.
+  def can_create_screen?
+    return false unless user
+    record.group.admin?(user)
+  end
+
+  # Screens can be edited by any member of the associated group.
+  def can_edit_screen?
+    return false unless user
+    record.group.member?(user)
+  end
+
+  # Screens can be updated by members of the group, but group changes
+  # require admin permissions on both the old and new groups.
+  def can_update_screen?
+    return false unless can_edit_screen?
+
+    # If group_id is being changed, ensure user is admin of both
+    # the current group and the new group.
+    if record.group_id_changed?
+      # Check permissions on the old group, if it existed.
+      if record.group_id_was.present?
+        old_group = Group.find_by(id: record.group_id_was)
+        return false unless old_group&.admin?(user)
+      end
+
+      # Check permissions on the new group.
+      # The `record` is already associated with the new group object.
+      return false unless record.group&.admin?(user)
+    end
+
+    true
+  end
+
+  # Screens can be deleted by any admin of the associated group.
+  def can_destroy_screen?
+    return false unless user
+    record.group.admin?(user)
+  end
+
+  public
 
   def permitted_attributes
     if can_edit_group?
