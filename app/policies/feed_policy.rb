47ca820cf925c84117bc5d9d61eaad4e4a0f50d0
Brian Michalski <bmichalski@gmail.com>
2025-12-14 23:46:35 -0500

    Build out feed policy, mirroring screen policy.

--- app/policies/feed_policy.rb
+++ app/policies/feed_policy.rb
@@ -27,17 +27,17 @@ class FeedPolicy < ApplicationPolicy
 
   def edit?
     # Once Feed has group_id, this will check group member permissions
-    super
+    super || can_edit_feed?
   end
 
   def update?
     # Once Feed has group_id, this will check group member permissions
-    super
+    super || can_update_feed?
   end
 
   def destroy?
     # Once Feed has group_id, this will check group admin permissions
-    super
+    super || can_destroy_feed?
   end
 
   def refresh?
@@ -58,6 +58,46 @@ class FeedPolicy < ApplicationPolicy
     user.admin_groups.any?
   end
 
+  # Feeds can be created by any admin of the associated group.
+  def can_create_feed?
+    return false unless user
+    record.group.admin?(user)
+  end
+
+  # Feeds can be edited by any member of the associated group.
+  def can_edit_feed?
+    return false unless user
+    record.group.member?(user)
+  end
+
+  # Feeds can be updated by members of the group, but group changes
+  # require admin permissions on both the old and new groups.
+  def can_update_feed?
+    return false unless can_edit_feed?
+
+    # If group_id is being changed, ensure user is admin of both
+    # the current group and the new group.
+    if record.group_id_changed?
+      # Check permissions on the old group, if it existed.
+      if record.group_id_was.present?
+        old_group = Group.find_by(id: record.group_id_was)
+        return false unless old_group&.admin?(user)
+      end
+
+      # Check permissions on the new group.
+      # The `record` is already associated with the new group object.
+      return false unless record.group&.admin?(user)
+    end
+
+    true
+  end
+
+  # Feeds can be deleted by any admin of the associated group.
+  def can_destroy_feed?
+    return false unless user
+    record.group.admin?(user)
+  end
+
   public
 
   def permitted_attributes
@@ -70,7 +110,7 @@ class FeedPolicy < ApplicationPolicy
     end
   end
 
-  # Helper method to determine if the user can edit the group which owns a screen.
+  # Helper method to determine if the user can edit the group which owns a feed.
   #
   # This is used both in the policy and in the view to disable UI elements.
   def can_edit_group?
